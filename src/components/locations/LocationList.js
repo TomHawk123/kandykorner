2326e09d25057550ed17f80d16547cf30d545f93
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-03-02 16:04:44 -0600

    added product and location list modules (with relevant directories) and displayed to DOM.

---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,27 @@
+
+
+import React, { useEffect, useState } from "react"
+
+export const LocationList = () => {
+    const [locations, setLocation] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/locations")
+                .then(res => res.json())
+                .then((locationArray) => {
+                    setLocation(locationArray)
+                })
+        },
+        []
+    )
+
+    return (
+        <>
+            <h2>Location List</h2>
+            {
+                locations.map(location => <p key={`location--${location.id}`}>{location.name}</p>)
+            }
+        </>
+    )
+}
